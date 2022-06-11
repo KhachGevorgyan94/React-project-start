f925c2e27fd3a2a235f5f91679bc07efd37de6fa
armansafaryan01 <armansafaryan75@gmail.com>
2022-06-12 02:58:47 +0400

    someto

--- src/Components/about-us/index.jsx
+++ src/Components/about-us/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import './stye.css'
+import Person from "../person";
+import './style.css'
+import person1 from "../assets/images/image-1.jpg";
 
 class AboutUs extends React.Component {
 
   render() {
-    return <div className='P-about-us'>
-      <h1> About us</h1>
+    return  <div className='P-about-us'>
+      <h1 className="header-h1"> About Simple House</h1>
+      <p className="header-p"> This is about page of simple house template.  You can modify and use this <br /> HTML template for your website. Total 3 HTML pages included in 
+        this template. <br /> Header image has a parallax effect.</p>
+        <div className="person-list">
+        <Person/>
+        </div>
     </div>
   }
 }
---
+++ src/Components/person/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css"
+
+class Person extends React.Component {
+    render() {
+        return <div className="person">
+            <div className="person-image"></div>
+            <div className="person-information">
+                <h2>Jennifer Soft</h2>
+                <h3>Founder and CEO</h3>
+                <p>Vivamus cursus leo nec sem feugiat <br />
+                    sagittis.  Duis ut feugiat  odio, sit amet <br /> accumsan odio.</p>
+                <div className="person-links">
+                    <ul>
+                        <li><a href="https://fb.com/">facebook</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    }
+
+
+}
+
+
+export default Person

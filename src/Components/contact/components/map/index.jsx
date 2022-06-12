7cc19d410ccbba511f17e6a4e9e96e32068ce57c
GoharH <hovsepyanga@gmail.com>
2022-06-13 00:42:30 +0300

    adding map

--- src/Components/contact/components/map/index.jsx
+++ src/Components/contact/components/map/index.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import './style.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class MapLocation extends React.Component {
     render() {
-        return
+        return <section>
+            <div className="container">
+                <div className="map">
+                    <Map google={this.props.google} zoom={14}>
+
+                        <Marker onClick={this.onMarkerClick}
+                            name={'Current location'} />
+
+                        <InfoWindow onClose={this.onInfoWindowClose}>
+                            <div>
+                                <h1>{this.state.selectedPlace.name}</h1>
+                            </div>
+                        </InfoWindow>
+                    </Map>
+                </div>
+            </div>
+        </section>
     }
 }
+export default GoogleApiWrapper({
+    apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+})(MapLocation)

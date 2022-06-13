eed4905c192d94d48215d83edc1dd78736ac97a1
armansafaryan01 <armansafaryan75@gmail.com>
2022-06-14 01:24:49 +0400

    add-lists

--- src/Components/about-us/index.jsx
+++ src/Components/about-us/index.jsx
@@ -4,15 +4,47 @@ import './style.css'
 import person1 from "../assets/images/image-1.jpg";
 
 class AboutUs extends React.Component {
+  state = {
+    list: [
+      {
+        name: "Daisy Walker",
+        work: "Executive Chef",
+        about: "Praesent non vulputate elit. Orci varius natoque et magnis dis parturient, nascetur ridiculus mus.",
+        image: "https://templatemo.com/templates/templatemo_539_simple_house/img/about-02.jpg",
+      },
 
+      {
+        name: "Daisy Walker 1",
+        work: "Executive Chef",
+        about: "Praesent non vulputate elit. Orci varius natoque et magnis dis parturient, nascetur ridiculus mus.",
+        image: "https://templatemo.com/templates/templatemo_539_simple_house/img/about-02.jpg",
+      },
+      {
+        name: "Daisy Walker 1",
+        work: "Executive Chef",
+        about: "Praesent non vulputate elit. Orci varius natoque et magnis dis parturient, nascetur ridiculus mus.",
+        image: "https://templatemo.com/templates/templatemo_539_simple_house/img/about-02.jpg",
+      },
+      {
+        name: "Daisy Walker 1",
+        work: "Executive Chef",
+        about: "Praesent non vulputate elit. Orci varius natoque et magnis dis parturient, nascetur ridiculus mus.",
+        image: "https://templatemo.com/templates/templatemo_539_simple_house/img/about-02.jpg",
+      },
+    ]
+  }
   render() {
-    return  <div className='P-about-us'>
+    return <div className='P-about-us'>
       <h1 className="header-h1"> About Simple House</h1>
-      <p className="header-p"> This is about page of simple house template.  You can modify and use this <br /> HTML template for your website. Total 3 HTML pages included in 
+      <p className="header-p"> This is about page of simple house template.  You can modify and use this <br /> HTML template for your website. Total 3 HTML pages included in
         this template. <br /> Header image has a parallax effect.</p>
-        <div className="person-list">
-        <Person/>
-        </div>
+      <div className="person-list">
+       
+        {this.state.list.map((item, index) => {
+          return  <Person data={item}  key={index}/>
+        })}
+
+      </div>
     </div>
   }
 }
--- src/Components/person/index.jsx
+++ src/Components/person/index.jsx
@@ -4,22 +4,23 @@ import "./style.css"
 class Person extends React.Component {
     render() {
         return <div className="person">
-            <div className="person-image"></div>
+            <div className="person-image">
+                <img src="" alt="" />
+            </div>
             <div className="person-information">
-                <h2>Jennifer Soft</h2>
-                <h3>Founder and CEO</h3>
-                <p>Vivamus cursus leo nec sem feugiat <br />
-                    sagittis.  Duis ut feugiat  odio, sit amet <br /> accumsan odio.</p>
+                <h2>{this.props.data.name}</h2>
+                <h3>{this.props.data.work}</h3>
+                <p>{this.props.data.about}</p>
                 <div className="person-links">
                     <ul>
-                         <li><a href="https://fb.com/">facebook</a></li>
+                         <li><a href="https://fb.com/" >facebook</a></li>
                          <li><a href="https://fb.com/">Twiter</a></li> 
                          <li><a href="https://fb.com/">Instagram</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-    }
+    } 
 
 
 }
